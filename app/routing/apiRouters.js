6d41cc8ddb0c60cee3a490e8fdc44149148b022f
Ksenia Kirilyuk <kseniakirilyuk@gmail.com>
2019-01-17 12:07:28 -0500

    best match part

--- app/routing/apiRouters.js
+++ app/routing/apiRouters.js
@@ -10,7 +10,38 @@ module.exports = function(app){
     
     app.post("/api/friends", function(req, res) {
         var newPerson = req.body;
+        var newScores = newPerson.scores;
+        var diff;
+
+        var match = {
+            name: "",
+            photo: "",
+            friendDifference: Infinity
+          };
+        
+          for (var i = 0; i < friendData.length; i++) {
+            var dataFriend = friendData[i];
+            diff= 0;
+      
+            console.log(dataFriend.name);
+      
+            for (var j = 0; j < dataFriend.scores.length; j++) {
+              var dataFriendScores = dataFriend.scores[j];
+              var userScores = newScores[j];
+      
+              diff += Math.abs(parseInt(userScores) - parseInt(dataFriendScores));
+            }
+      
+            if (diff <= match.friendDifference) {
+              match.name = dataFriend.name;
+              match.photo = dataFriend.photo;
+              match.friendDifference = diff;
+            }
+          }
+      
         friendData.push(newPerson); 
-        console.log("new person is in API")     
+        console.log("new person is in API")    
+        res.json(match);
+        
       });
 };
