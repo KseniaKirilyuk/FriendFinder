16211406054db9229b2455e1f8a98ce4157b17f0
Ksenia Kirilyuk <kseniakirilyuk@gmail.com>
2019-01-17 23:42:21 -0500

    fixed style link

--- app/routing/htmlRouters.js
+++ app/routing/htmlRouters.js
@@ -3,8 +3,8 @@ var path = require("path");
 //routing
 module.exports = function(app) {
     app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
+        res.sendFile(path.join(__dirname, "/../public/survey.html"));
       });
     
-}
+};
 
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static("public"));
 //routing
 require("./app/routing/apiRouters")(app);
 require("./app/routing/htmlRouters")(app);
